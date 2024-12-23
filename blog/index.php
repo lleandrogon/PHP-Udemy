b7fa75e4ac679d15f6c0e63148c244353c33f55d
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-12-22 23:41:08 -0300

    Estruturas de repetição

--- blog/index.php
+++ blog/index.php
@@ -6,5 +6,15 @@
     require_once "sistema/configuracao.php";
     include_once "helpers.php";
 
-    echo saudacao();
+    $numero = 6;
+
+    /* while ($numero <= 10) {
+        echo $numero++;
+    } */
+
+    for ($i = 1; $i <= 10; $i++) {
+        // echo ($i % 2 ? $i . " ímpar" : $i . " par") . "<br>";
+        
+        // echo $i . " x " . $numero . " = " . $i * $numero . "<br>";
+    }
 ?>
