70236f1e1e6dae0f51cb2fef7c23a5674a5ca1ee
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-12-24 15:06:57 -0300

    Introdução ao composer

--- blog/index.php
+++ blog/index.php
@@ -1,20 +1,8 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 <?php
-    //Arquivo index responsável pela inicialização do sistema
 
     //declare(strict_types = 1); 
 
-    require_once "sistema/configuracao.php";
-    include_once "helpers.php";
-    include "./sistema/Nucleo/Mensagem.php";
+    require 'vendor/autoload.php';
 
-    echo (new Mensagem())->alerta("texto de alerta");
-
-    // $msg = new Mensagem();
-    // echo $msg->sucesso("Mensagem de sucesso")->renderizar();
-
-    // echo (new Mensagem())->erro("mensagem de erro")->renderizar();
-
-    echo "<hr>";
+    echo SITE_NOME;
 ?>
---
+++ blog/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ceounset/cursophp8',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dd2e4dd96531ade83a535801c07644dbaa8219dc',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ceounset/cursophp8' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dd2e4dd96531ade83a535801c07644dbaa8219dc',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
